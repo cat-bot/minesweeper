36eadaff9159fea69931714fc906fd72ec41e478
Flynn Longley <oolong.singy@gmail.com>
2021-05-25 19:48:48 +1200

    gold plating!

--- js/minesweeper/modules/constants.js
+++ js/minesweeper/modules/constants.js
@@ -3,7 +3,7 @@
 const GRID_SIZES = {
     beginner : { width: 9, height: 9, mines: 10, label: "beginner"},
     intermediate: { width: 16, height: 16, mines: 40, label: "intermediate"},
-    expert: { width: 30, height: 16, mines: 99, label: "expert"},
+    expert: { width: 16, height: 26, mines: 86, label: "expert"},
 };
 
 const GAME_COMPLETION_STATES = {
